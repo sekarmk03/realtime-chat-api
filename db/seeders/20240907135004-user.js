b415f30c87eb671251e76a2082122be1564fbedf
Sekar Madu Kusumawardani <sekarmadu99@gmail.com>
2024-09-07 20:56:16 +0700

    setup: add user model, migration, seeder

---
+++ db/seeders/20240907135004-user.js
@@ -0,0 +1,28 @@
+'use strict';
+const bcrypt = require('bcrypt');
+
+/** @type {import('sequelize-cli').Migration} */
+module.exports = {
+  async up (queryInterface, Sequelize) {
+    await queryInterface.bulkInsert('users', [
+      {
+        name: 'Chester Bennington',
+        email: 'chester@example.com',
+        password: await bcrypt.hash('chester123', 10),
+        created_at: new Date("2024-09-06T08:50:04.000Z"),
+        updated_at: new Date("2024-09-06T08:50:04.000Z")
+      },
+      {
+        name: 'Mike Shinoda',
+        email: 'mike@example.com',
+        password: await bcrypt.hash('mike123', 10),
+        created_at: new Date("2024-09-07T13:50:04.000Z"),
+        updated_at: new Date("2024-09-07T13:50:04.000Z")
+      },
+    ], {});
+  },
+
+  async down (queryInterface, Sequelize) {
+    await queryInterface.bulkDelete('users', null, {});
+  }
+};
